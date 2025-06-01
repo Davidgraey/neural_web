ca08c725364e4742927d75c082383c1f2678f152
David Graey <graeyband@gmail.com>
2025-06-01 14:55:26 -0700

    sketch updates

--- neural_web/cells/soma.py
+++ neural_web/cells/soma.py
@@ -6,3 +6,9 @@ from organelle import Organelle
 
 class Soma(Organelle):
     pass
+
+
+class SoamticAttention:
+    """ RoPE position, Latent Fourier Attention """
+    def __init__(self):
+        pass
